f7052dccf9afc10b58b97a929a3ffd383c32bf82
kerimabdy <kerim060620@gmail.com>
2025-02-24 17:16:26 +0500

    Introduce Constants and Destination objects for application-wide values
    This commit introduces two new objects, `Constants` and `Destination`, to manage application-wide constants and navigation destinations, respectively.
    
    -   Adds `Constants` object with `MUSIC_SERVICE_SCOPE` and `MUSIC_COLLECTION` values for use throughout the application.
    -   Adds `Destination` object with `home` and `musicScreen` values to define navigation routes.

---
+++ app/src/main/java/tm/app/musicplayer/other/Constants.kt
@@ -0,0 +1,6 @@
+package tm.app.musicplayer.other
+
+object Constants {
+    const val MUSIC_SERVICE_SCOPE = "MusicServiceScope"
+    const val MUSIC_COLLECTION = "musics"
+}
---
+++ app/src/main/java/tm/app/musicplayer/ui/navigation/Destination.kt
@@ -0,0 +1,6 @@
+package tm.app.musicplayer.ui.navigation
+
+object Destination {
+    const val home = "home"
+    const val musicScreen = "musicScree"
+}
