1a7c812cdc3c6fa41327be3ea4850057354e8099
kerimabdy <kerim060620@gmail.com>
2025-02-28 18:02:50 +0500

    Configure MediaSession to use the app launch intent and implement MediaSession.Callback
    This commit updates the `MusicService` to configure the `MediaSession` with the application's launch intent and adds a `MediaSession.Callback` implementation.
    
    -   Configures `MediaSession` to set the `sessionActivity` to the app launch `PendingIntent`.
    -   Adds a `MediaSessionCallback` class that implements `MediaSession.Callback` and overwrites the `onAddMediaItems` method.
    -   Updates the `onAddMediaItems` method to transform the media item's URI to it's mediaId.

--- app/src/main/java/tm/app/musicplayer/data/service/MusicService.kt
+++ app/src/main/java/tm/app/musicplayer/data/service/MusicService.kt
@@ -1,5 +1,7 @@
 package tm.app.musicplayer.data.service
 
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
@@ -8,6 +10,7 @@ import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
 import org.koin.android.ext.android.getKoin
 import org.koin.core.qualifier.named
+import tm.app.musicplayer.MainActivity
 import tm.app.musicplayer.other.Constants.MUSIC_SERVICE_SCOPE
 
 class MusicService: MediaSessionService() {
@@ -22,14 +25,14 @@ class MusicService: MediaSessionService() {
 
     override fun onCreate() {
         super.onCreate()
-//        val fallbackIntent = Intent(this, MainActivity::class.java)
-//        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
-//            PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_IMMUTABLE)
-//        } ?: PendingIntent.getActivity(this, 0, fallbackIntent, PendingIntent.FLAG_IMMUTABLE)
+        val fallbackIntent = Intent(this, MainActivity::class.java)
+        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
+            PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_IMMUTABLE)
+        } ?: PendingIntent.getActivity(this, 0, fallbackIntent, PendingIntent.FLAG_IMMUTABLE)
 
         // Initialize MediaSession (MediaSessionConnector is no longer needed)
         mediaSession = MediaSession.Builder(this, exoPlayer)
-//            .setSessionActivity(activityIntent) // Set the app launch intent
+            .setSessionActivity(activityIntent) // Set the app launch intent
 //            .setCallback(MediaSessionCallback())
             .build()
 
@@ -49,18 +52,18 @@ class MusicService: MediaSessionService() {
         super.onDestroy()
     }
 
-//    private inner class MediaSessionCallback: MediaSession.Callback {
-//        override fun onAddMediaItems(
-//            mediaSession: MediaSession,
-//            controller: MediaSession.ControllerInfo,
-//            mediaItems: MutableList<MediaItem>
-//        ): ListenableFuture<MutableList<MediaItem>> {
-//            val updatedMediaItems = mediaItems.map {
-//                it.buildUpon().setUri(it.mediaId).build()
-//            }.toMutableList()
-//
-//            return Futures.immediateFuture(updatedMediaItems)
-//        }
-//    }
+    private inner class MediaSessionCallback: MediaSession.Callback {
+        override fun onAddMediaItems(
+            mediaSession: MediaSession,
+            controller: MediaSession.ControllerInfo,
+            mediaItems: MutableList<MediaItem>
+        ): ListenableFuture<MutableList<MediaItem>> {
+            val updatedMediaItems = mediaItems.map {
+                it.buildUpon().setUri(it.mediaId).build()
+            }.toMutableList()
+
+            return Futures.immediateFuture(updatedMediaItems)
+        }
+    }
 
 }
