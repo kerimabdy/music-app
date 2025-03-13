b1e8965c00285a4b04d30b228ecdaf02c673c4d1
kerimabdy <kerim060620@gmail.com>
2025-03-13 09:33:46 +0500

    Refactor MusicService package location and update MainActivity
    This commit moves the `MusicService` from the `data.service` package to the `exoplayer` package. It also updates the `MainActivity` to reflect this change by modifying the import statement.
    
    -   Moves `MusicService` to the `exoplayer` package.
    -   Updates the import for `MusicService` in `MainActivity`.

--- app/src/main/java/tm/app/musicplayer/MainActivity.kt
+++ app/src/main/java/tm/app/musicplayer/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import org.koin.androidx.compose.KoinAndroidContext
 import org.koin.androidx.compose.koinViewModel
-import tm.app.musicplayer.data.service.MusicService
+import tm.app.musicplayer.exoplayer.MusicService
 import tm.app.musicplayer.ui.MusicPlayerApp
 import tm.app.musicplayer.ui.theme.MusicPlayerTheme
 import tm.app.musicplayer.ui.viewmodels.SharedViewModel
