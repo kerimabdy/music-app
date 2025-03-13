455ef6c4046a99efb0e891997eec4d2d0ea892e9
kerimabdy <kerim060620@gmail.com>
2025-03-13 09:35:55 +0500

    Refactor MusicPlayerApp to use Scaffold and add navigation tabs
    This commit refactors the `MusicPlayerApp` to use a `Scaffold` for a more structured layout and introduces navigation tabs.
    
    -   Replaces previous layout with `Scaffold` for better structure.
    -   Adds `TopAppBar` with `exitUntilCollapsedScrollBehavior`.
    -   Implements `HomeTabRow` with navigation tabs.
    -   Adds `TopLevelRoute` data class to handle navigation routes.
    -   Updates `MusicPlayerNavHost` to use new navigation destinations: `Packs`, `Tracks`, and `Downloads`.
    -   Updates `MusicPlayerNavHost` to use a modifier.
    - Adds `PacksScreen`
    - Adds `PacksViewModel`
    - Added new dependencies to implement the changes.

--- app/src/main/java/tm/app/musicplayer/ui/MusicPlayerApp.kt
+++ app/src/main/java/tm/app/musicplayer/ui/MusicPlayerApp.kt
@@ -1,84 +1,171 @@
 package tm.app.musicplayer.ui
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import org.koin.androidx.compose.koinViewModel
-import tm.app.musicplayer.ui.home.HomeEvent
-import tm.app.musicplayer.ui.home.HomeScreen
-import tm.app.musicplayer.ui.home.HomeViewModel
-import tm.app.musicplayer.ui.home.component.HomeBottomBar
-import tm.app.musicplayer.ui.musicscreen.MusicScreen
-import tm.app.musicplayer.ui.musicscreen.MusicViewModel
-import tm.app.musicplayer.ui.navigation.Home
-import tm.app.musicplayer.ui.navigation.MusicScreen
+import tm.app.musicplayer.ui.navigation.components.HomeAppBar
+import tm.app.musicplayer.ui.navigation.Downloads
+import tm.app.musicplayer.ui.navigation.Packs
+import tm.app.musicplayer.ui.navigation.TopLevelRoute
+import tm.app.musicplayer.ui.navigation.Tracks
+import tm.app.musicplayer.ui.navigation.components.HomeTabItem
+import tm.app.musicplayer.ui.navigation.components.HomeTabRow
+import tm.app.musicplayer.ui.packs.PacksScreen
+import tm.app.musicplayer.ui.packs.PacksViewModel
 import tm.app.musicplayer.ui.viewmodels.SharedViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MusicPlayerApp(sharedViewModel: SharedViewModel) {
     val navController = rememberNavController()
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
 
-    MusicPlayerNavHost(
-        navController = navController,
-        sharedViewModel = sharedViewModel
+    val topLevelRoutes = listOf(
+        TopLevelRoute("Packs", Packs),
+        TopLevelRoute("Tracks", Tracks),
+        TopLevelRoute("Downloads", Downloads)
     )
+
+    Scaffold(
+        modifier = Modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            Column {
+                HomeAppBar(
+                    scrollBehavior = scrollBehavior
+                )
+                HomeTabRow(
+                    selectedTabIndex = { selectedTabIndex }
+                ) {
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentDestination = navBackStackEntry?.destination
+                    topLevelRoutes.forEachIndexed { index, topLevelRoute ->
+                        Log.d("DestinationNames", currentDestination?.route ?: "")
+                        Log.d("RouteNames", topLevelRoute.route::class.qualifiedName ?: "")
+
+                        HomeTabItem(
+                            label = topLevelRoute.name,
+                            onClick = {
+                                navController.navigate(topLevelRoute.route) {
+                                    // Pop up to the start destination of the graph to
+                                    // avoid building up a large stack of destinations
+                                    // on the back stack as users select items
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    // Avoid multiple copies of the same destination when
+                                    // reselecting the same item
+                                    launchSingleTop = true
+                                    // Restore state when reselecting a previously selected item
+                                    restoreState = true
+                                }
+                            },
+                            isSelected =  currentDestination?.route == topLevelRoute.route::class.qualifiedName
+                        )
+                    }
+                }
+            }
+        }
+    ) { innerPadding ->
+        MusicPlayerNavHost(
+            modifier = Modifier.padding(innerPadding),
+            navController = navController,
+            sharedViewModel = sharedViewModel
+        )
+    }
+
+
 }
 
 @Composable
-fun MusicPlayerNavHost(navController: NavHostController, sharedViewModel: SharedViewModel) {
+fun MusicPlayerNavHost(
+    navController: NavHostController,
+    sharedViewModel: SharedViewModel,
+    modifier: Modifier = Modifier
+) {
     val musicControllerUiState = sharedViewModel.musicControllerUiState
-
-
     NavHost(
         navController = navController,
-        startDestination = Home
+        startDestination = Packs,
+//        popExitTransition = {
+//            scaleOut(
+//                targetScale = 0.9f,
+//                transformOrigin = TransformOrigin(pivotFractionX = 0.5f, pivotFractionY = 0.5f)
+//            )
+//        },
+//        popEnterTransition = {
+//            EnterTransition.None
+//        },
     ){
-        composable<Home> {
-            val mainViewModel = koinViewModel<HomeViewModel>()
-            val isInitialized = rememberSaveable { mutableStateOf(false) }
 
-            if (!isInitialized.value) {
-                LaunchedEffect(Unit) {
-                    mainViewModel.onEvent(HomeEvent.FetchMusic)
-                    isInitialized.value = true
-                }
-            }
-
-            Box(modifier = Modifier.fillMaxSize()) {
-                HomeScreen(
-                    onEvent = mainViewModel::onEvent,
-                    uiState = mainViewModel.homeUiState,
-                )
-                HomeBottomBar(
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter),
-                    onEvent = mainViewModel::onEvent,
-                    playerState = musicControllerUiState.playerState,
-                    uiState = mainViewModel.homeUiState,
-                    onBarClick = { navController.navigate(MusicScreen) }
-                )
+        composable<Packs> {
+            val packsViewModel = koinViewModel<PacksViewModel>()
+            PacksScreen(modifier = modifier, uiState = packsViewModel.packsUiState)
+        }
+        composable<Tracks> {
+            Column(modifier = modifier)  {
+                Text("Tracks")
             }
         }
-
-        composable<MusicScreen> {
-            val musicViewModel: MusicViewModel = koinViewModel<MusicViewModel>()
-            MusicScreen(
-                onEvent = musicViewModel::onEvent,
-                musicControllerUiState = musicControllerUiState,
-                onNavigateUp = { navController.navigateUp() }
-            )
+        composable<Downloads> {
+            Column(modifier = modifier)  {
+                Text("Downloads")
+            }
         }
-
-
+//        composable<Home> {
+//            val mainViewModel = koinViewModel<HomeViewModel>()
+//            val isInitialized = rememberSaveable { mutableStateOf(false) }
+//
+//            if (!isInitialized.value) {
+//                LaunchedEffect(Unit) {
+//                    mainViewModel.onEvent(HomeEvent.FetchMusic)
+//                    isInitialized.value = true
+//                }
+//            }
+//
+//            Box(modifier = Modifier.fillMaxSize()) {
+//                HomeScreen(
+//                    onEvent = mainViewModel::onEvent,
+//                    uiState = mainViewModel.homeUiState,
+//                )
+//                HomeBottomBar(
+//                    modifier = Modifier
+//                        .align(Alignment.BottomCenter),
+//                    onEvent = mainViewModel::onEvent,
+//                    playerState = musicControllerUiState.playerState,
+//                    uiState = mainViewModel.homeUiState,
+//                    onBarClick = { navController.navigate(MusicScreen) }
+//                )
+//            }
+//        }
+//
+//        composable<MusicScreen> {
+//            val musicViewModel: MusicViewModel = koinViewModel<MusicViewModel>()
+//            MusicScreen(
+//                onEvent = musicViewModel::onEvent,
+//                musicControllerUiState = musicControllerUiState,
+//                onNavigateUp = { navController.navigateUp() }
+//            )
+//        }
     }
 }
